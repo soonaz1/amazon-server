5efc9fe25f30885aca53e8b8945d1ffe7837ec0f
Hussein Elsayed <hussein.elsayed96@gmail.com>
2023-01-09 16:45:20 +0200

    test

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // Package Imports
-const express = require('express'); 
-const mongoose = require('mongoose');
-const adminRouter = require('./routes/admin');
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
 
 //File Imports
-const authRouter= require('./routes/auth');
-const productRouter = require('./routes/product');
-const userRouter = require('./routes/user');
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
 
 //Initializations
-const PORT = 3000; // port 
+const PORT = process.env.PORT || 3000; // port
 const app = express();
-const DB = 'mongodb+srv://soonaz:2580@cluster0.zk1pzsx.mongodb.net/?retryWrites=true&w=majority';
- 
-// middleware 
+const DB =
+  "mongodb+srv://soonaz:2580@cluster0.zk1pzsx.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
 app.use(express.json());
 app.use(authRouter);
 app.use(adminRouter);
 app.use(productRouter);
 app.use(userRouter);
 
-// Connections 
+// Connections
 mongoose
   .connect(DB)
   .then(() => {
@@ -30,5 +31,6 @@ mongoose
     console.log(e);
   });
 
-app.listen(PORT,"0.0.0.0", ()=>{
-console.log(`Connected at ${PORT}`);});
+app.listen(PORT, () => {
+  console.log(`Connected at ${PORT}`);
+});
